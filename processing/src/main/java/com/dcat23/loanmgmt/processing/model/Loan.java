232549e01bbd5691279e05d6b5ed80e1742942dc
Devin Catuns <devincatuns1@gmail.com>
2024-07-21 04:02:47 -0400

    create loan processing entities

---
+++ processing/src/main/java/com/dcat23/loanmgmt/processing/model/Loan.java
@@ -0,0 +1,39 @@
+package com.dcat23.loanmgmt.processing.model;
+
+import com.dcat23.loanmgmt.core.model.LoanStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "loans")
+public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long loanId;
+
+
+    @Column(nullable = false, name = "customer_id")
+    private Long customerId; // Reference to the customer in User Service
+
+    @Column(nullable = false, name = "amount")
+    private Double loanAmount;
+
+    @Column(nullable = false, name = "interest_rate")
+    private Double interestRate;
+
+    @Column(nullable = false, name = "term")
+    private Integer loanTerm;
+
+    @Column(nullable = false, name = "start_date")
+    private LocalDate startDate;
+
+    @Column(nullable = false, name = "end_date")
+    private LocalDate endDate;
+
+    @Column(nullable = false, name = "status")
+    private LoanStatus status;
+}
